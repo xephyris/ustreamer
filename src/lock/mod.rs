eed24abdb5a91d641d050de1645700b68b8302f1
xephyris <114379647+xephyris@users.noreply.github.com>
2025-08-28 23:51:51 -0700

    feat: remove all older instances of ustreamer on init.

--- src/lock/mod.rs
+++ src/lock/mod.rs
@@ -8,28 +8,31 @@ pub struct StreamLock {
 impl StreamLock {
     pub fn aquire_lock(path: String) -> StreamLock {
         let pid = std::process::id();
-        let timestamp = chrono::Utc::now().to_string();
+        let target_name = "ustreamer";
 
-        let lock_str = format!("{}\n{}", pid, timestamp);
+        let lock_str = format!("{}\n", pid);
         if std::path::Path::exists(&std::path::PathBuf::from(&path)) {
             let file = std::fs::read_to_string(&path).unwrap();
             let mut read = file.lines();
             println!("Old streamer found, killing streamer");
-            if let Ok(old_timestamp) = read.next().unwrap_or("").to_owned().parse::<DateTime<Utc>>()
-                && let Ok(old_pid) = read.next().unwrap_or("").to_owned().parse::<u32>() {
-                if Utc::now() > old_timestamp {
-                    if let Some(process) = sysinfo::System::new().process(sysinfo::Pid::from_u32(old_pid)) {
-                        println!("Killing ustreamer at PID {}", old_pid);
-                        sysinfo::Process::kill(process);
+            if let Ok(old_pid) = read.next().unwrap_or("").to_owned().parse::<u32>() {
+                let mut system = sysinfo::System::new_all();
+                system.refresh_processes(sysinfo::ProcessesToUpdate::All, true);
+
+               for (pid, process) in system.processes() {
+                    if process.name() == target_name && pid.as_u32() != std::process::id(){
+                        println!("Killing '{}' at PID {}", target_name, pid);
+                        process.kill();
                     }
                 }
             }
         }
-
         println!("Writing to lock file");
         if let Err(e) = std::fs::write(&path, lock_str) {
             println!("Lock failed with error: {}", e);
         }
+
+        
 
         StreamLock {
             path
