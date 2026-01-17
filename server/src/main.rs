6898ef7061ca7675e31bbfa72a469f03287de37c
xephyris <114379647+xephyris@users.noreply.github.com>
2026-01-16 23:57:42 -0800

    wip: fixing mjpeg stream for axum web server non unix mode

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,5 @@
 use axum::{
-    Extension, Router, body::{Body, BodyDataStream}, http::header::{CACHE_CONTROL, CONNECTION, CONTENT_TYPE, EXPIRES, PRAGMA, TRANSFER_ENCODING}, response::{Html, Response}, routing::get
+    Extension, Router, body::{Body, BodyDataStream}, http::{Uri, header::{CACHE_CONTROL, CONNECTION, CONTENT_TYPE, EXPIRES, PRAGMA, TRANSFER_ENCODING}}, response::{Html, Response}, routing::get
 };
 use byteorder::LittleEndian;
 use bytes::Bytes;
@@ -37,7 +37,7 @@ async fn main() {
     eprintln!("Removing old socket...");
     std::fs::remove_file(socket_path).ok();
 
-    let unix = true;
+    let unix = false;
     eprintln!("Binding to new socket");
 
 
@@ -72,10 +72,11 @@ async fn main() {
         }).await.unwrap();
     } else {
         let app = Router::new() .route("/", get(mjpeg_html))
-            .route("/stream", get(mjpeg_page))
+                .route("/stream", get(mjpeg_page))
             .route("/state", get(streamer_details))
             .route("/snapshot", get(snapshot_handler))
-            .layer(Extension(shared_clone.clone()));
+            .layer(Extension(shared_clone.clone()))
+            .layer(Extension(client_list.clone()));
 
         let (tx, rx) = mpsc::channel::<bool>();
 
@@ -124,9 +125,12 @@ async fn attach_socket(image_data: Arc<RwLock<ImageData>>) {
 
 type SharedImage = Arc<RwLock<Option<Vec<u8>>>>;
 
-async fn mjpeg_page(image: Extension<Arc<RwLock<ImageData>>>) -> Response {
+async fn mjpeg_page(req: Uri, image: Extension<Arc<RwLock<ImageData>>>, client_list: Extension<Arc<RwLock<Clients>>>) -> Response {
+    let line = if let Some(header) = req.path_and_query() {header.to_string()} else {"/stream".to_string()};
+    println!("{}", line);
     let stream = stream::unfold((), move |_| {
         let image = image.clone();
+        
         // let value = sock_stream.clone();
         async move {
             // sleep(Duration::from_millis(20)).await;
@@ -169,6 +173,190 @@ async fn mjpeg_page(image: Extension<Arc<RwLock<ImageData>>>) -> Response {
             }
         }
     });
+
+    // tokio::spawn(async move 
+    //     {
+
+    //         let stream_shared = image.clone();
+    //         let client_clone = client_list.clone();
+    //         let _c_id = client_clone.write().await.add_client_from_header(line.clone());
+            
+
+
+    //         let headers = format!(
+    //             "HTTP/1.1 200 OK\r\n\
+    //             Cache-Control: no-store, no-cache, must-revalidate, proxy-revalidate, pre-check=0, post-check=0, max-age=0\r\n\
+    //             Pragma: no-cache\r\n\
+    //             Expires: Mon, 3 Jan 2000 12:34:56 GMT\r\n\
+    //             Set-Cookie: stream_client={}/{}; path=/; max-age=30\r\n\
+    //             Connection: keep-alive\r\n\
+    //             Content-Type: multipart/x-mixed-replace;boundary=boundarydonotcross\r\n\r\n",
+    //             _c_id.1, _c_id.0
+    //         );
+
+    //         writer.write_all(headers.as_bytes()).await.unwrap();
+    //         writer.flush().await.unwrap();
+
+    //         // println!("Sent header {}", headers);
+
+    //         let mut prev_frame = None;
+    //         // let mut interval = tokio::time::interval(Duration::from_millis(33));
+    //         let mut first = true;
+    //         let mut count = 0;
+
+    //         let mut skip = false;
+    //         let mut fps = 0;
+    //         let mut start = Instant::now();
+    //         let mut avg_frame_time = 0;
+    //         let mut df_frame_sent = false;
+    //         let (dual_final_frame, advance_headers, extra_headers, zero_data) = {
+    //             if let Some((dff, ah, eh, zd)) = client_clone.read().await.get_client_settings(Some(_c_id.1)) {
+    //                 (dff, ah, eh, zd)
+    //             } else {
+    //                 (false, false, false, false)
+    //             }
+    //         };
+    //         loop {
+    //             let frame_time = Instant::now();
+    //             // interval.tick().await;
+    //             count += 1;
+
+    //             if start.elapsed().as_millis() > 1000 {
+    //                 println!("WEB SERVER FRAME TIME {}", avg_frame_time);
+    //                 start = Instant::now();
+    //                 client_clone.write().await.update_fps_from_header(line.clone(), fps);
+    //                 fps = 0;
+    //             }
+    //             fps += 1;
+    //             let mut frame = Vec::new();
+    //             if prev_frame.is_none() {
+    //                 let lock = stream_shared.read().await;
+    //                 let img = {
+    //                     lock.frame.clone().unwrap_or(Vec::new())
+    //                 };
+    //                 // println!("img lock acquired parent {}/{}", _c_id.1, _c_id.0);
+    //                 let timestamp = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs_f64();
+    //                 frame = Vec::new();
+    //                 // TODO Temporarily incorrect, add proper implementation 
+    //                 if !advance_headers {
+    //                     frame.extend_from_slice(format!(
+    //                         "--boundarydonotcross\r\n\
+    //                         Content-Type: image/jpeg\r\n\
+    //                         Content-Length: {}\r\n\
+    //                         X-Timestamp: {:.6}\r\n\r\n",
+    //                         // img.as_ref().map_or(0, |i| i.len()),
+    //                         img.len(),
+    //                         timestamp
+    //                     ).as_bytes());
+    //                 } else {
+    //                     frame.extend_from_slice(format!(
+    //                         "--boundarydonotcross\r\n\
+    //                         Content-Type: image/jpeg\r\n\
+    //                         Content-Length: {}\r\n\r\n",
+    //                         img.len(),
+    //                     ).as_bytes());
+    //                 }
+
+    //                 // println!("frame header: {}", String::from_utf8(frame.clone()).unwrap());
+    //                 // if let Some(img) = img {
+    //                     frame.extend_from_slice(&img);
+    //                 // }
+
+    //                 frame.extend_from_slice(b"\r\n");
+
+    //                 prev_frame.replace(frame.clone());
+    //             } else {
+    //                 let lock = tokio::time::timeout(Duration::from_millis(50), stream_shared.read()).await;
+    //                 if let Ok(lock) = lock {
+    //                     // println!("SKIP STATUS: {}", lock.skip);
+    //                     if lock.skip == false {
+    //                         skip = false;
+    //                         let img = lock.frame.clone().unwrap();
+    //                         // println!("img length:{}", img.len());
+    //                         // println!("img lock acquired parent {}/{}", _c_id.1, _c_id.0);
+    //                         let timestamp = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs_f64();
+    //                         frame = Vec::new();
+    //                         // TODO Temporarily incorrect, add proper implementation 
+    //                         if !advance_headers {
+    //                             frame.extend_from_slice(format!(
+    //                                 "--boundarydonotcross\r\n\
+    //                                 Content-Type: image/jpeg\r\n\
+    //                                 Content-Length: {}\r\n\
+    //                                 X-Timestamp: {:.6}\r\n\r\n",
+    //                                 // img.as_ref().map_or(0, |i| i.len()),
+    //                                 img.len(),
+    //                                 timestamp
+    //                             ).as_bytes());
+    //                         } else {
+    //                             frame.extend_from_slice(format!(
+    //                                 "--boundarydonotcross\r\n\
+    //                                 Content-Type: image/jpeg\r\n\
+    //                                 Content-Length: {}\r\n",
+    //                                 img.len(),
+    //                             ).as_bytes());
+    //                         }
+
+    //                         // println!("frame header: {}", String::from_utf8(frame.clone()).unwrap());
+    //                         // if let Some(img) = img {
+    //                             frame.extend_from_slice(&img);
+    //                         // }
+
+    //                         frame.extend_from_slice(b"\r\n");
+    //                         prev_frame.replace(frame.clone());
+    //                     } else {
+    //                         skip = true;
+    //                     }
+    //                 } else {
+    //                     frame = prev_frame.as_ref().unwrap().clone();
+    //                     // println!("using previous image because lock was not acquired");
+    //                 }
+    //             }
+
+    //             // println!("frame data ready and sending");
+    //             // println!("FRAMES RECIEVED AND PROCESSED COUNT:     {}", count);
+    //             if !skip {
+    //                 df_frame_sent = false;
+    //                 if let Err(e) = writer.write_all(&frame).await {
+    //                     eprintln!("Failed connection, {}", e);
+    //                     break;
+    //                 }
+    //                 if let Err(e) = writer.flush().await {
+    //                     eprintln!("Failed flush, {}", e);
+    //                     break;
+    //                 }
+
+    //                 tokio::time::sleep(Duration::from_millis(10)).await;
+
+    //                 if first {
+    //                     first = false;
+    //                     if let Some(client) = client_clone.write().await.get_client_from_header(line.clone()) {
+    //                         client.update_fps(30);
+    //                     } else {
+    //                         break;
+    //                     }
+    //                 }
+    //                 if avg_frame_time != 0 {
+    //                     avg_frame_time = (avg_frame_time + frame_time.elapsed().as_millis())/2;
+    //                 } else {
+    //                     avg_frame_time = frame_time.elapsed().as_millis();
+    //                 } 
+    //             } else if !df_frame_sent && dual_final_frame && let Some(ref prev_frame) = prev_frame{
+    //                 df_frame_sent = true;
+    //                 if let Err(e) = writer.write_all(&prev_frame).await {
+    //                     eprintln!("Failed conneciton, {}", e);
+    //                     break;
+    //                 }
+    //                 if let Err(e) = writer.flush().await {
+    //                     eprintln!("Failed flush, {}", e);
+    //                     break;
+    //                 }
+    //             } else {
+    //                 println!("Skipping identical frame");
+    //             }
+    //         }
+    //         client_clone.write().await.remove_client_from_header(line.clone());
+    //     }
+    // );
 
     Response::builder()
         .status(200)
